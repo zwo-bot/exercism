e2aef9a22d4fe2f40339b50c775a60ef250a7407
Marco Messerschmidt <marco@38911.net>
2020-08-30 14:00:36 +0200

    hamming
    Signed-off-by: Marco Messerschmidt <marco@38911.net>

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,18 +1,16 @@
-// Calulates the hamming distance between to strings
+// Package hamming calulates the hamming distance between to strings
 package hamming
 
 import (
 	"errors"
 )
 
+// Distance calulates the hamming distance between to strings
 func Distance(a, b string) (int, error) {
-
 	var distance int
-
 	if len(a) != len(b) {
 		return 0, errors.New("Strings are not equal length")
 	}
-
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			distance++
